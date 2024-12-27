f947896782812aea9cd9cba114f66afabb64d734
Aayush61000 <ayushbro94184@gmail.com>
2024-12-28 00:32:19 +0530

    register captain done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -10,6 +10,12 @@ module.exports.registerUser = async(req,res,next) => {
     }
     
     const { fullname, email, password } = req.body;
+
+    const isUserAlreadyExist = await userModel.findOne({ email });
+    if (isUserAlreadyExist) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
+
     const hashedPassword = await userModel.hashPassword(password);
 
     const user = await userService.createUser({
@@ -18,6 +24,7 @@ module.exports.registerUser = async(req,res,next) => {
         email,
         password: hashedPassword
     });
+
 
     const token = user.generateAuthToken();
 
